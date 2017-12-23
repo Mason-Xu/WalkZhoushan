ecc6fbff78225cc673ad40998d48245db646cd53
Mason-Xu <xulihao1997@gmail.com>
2017-12-23 11:23:19 +0800

    add addtravelnote page1 test

--- app/src/main/java/com/example/walkzhoushan/Test.java
+++ app/src/main/java/com/example/walkzhoushan/Test.java
@@ -11,7 +11,7 @@ public class Test extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.travelnote_scroll);
+        setContentView(R.layout.add_travelnote1);
 
     }
 }
