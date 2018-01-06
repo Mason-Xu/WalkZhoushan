59eb9acae1c2af1fda4fbddabc0f7d436b6d271a
Mason-Xu <xulihao1997@gmail.com>
2018-01-06 14:46:24 +0800

    :sparkles: 添加了游记发表功能,没有完善

--- app/src/main/java/com/example/walkzhoushan/Travelnote_add.java
+++ app/src/main/java/com/example/walkzhoushan/Travelnote_add.java
@@ -1,11 +1,28 @@
 package com.example.walkzhoushan;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Mason on 2017/12/24.
@@ -14,8 +31,13 @@ import android.widget.Button;
 public class Travelnote_add extends AppCompatActivity implements View.OnClickListener {
     private Button back;
     private Button travelnote_addtext1;
-    private Button travelnote_addtext2;
-    private Button travelnote_addimg;
+    private Button release_travelnote;
+    private Button chooseFromAlbum;
+    private ImageView picture;
+
+    private Uri imageUri;
+
+    public static final int CHOOSE_PHOTO = 2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,30 +46,46 @@ public class Travelnote_add extends AppCompatActivity implements View.OnClickLis
 
         back = (Button) findViewById(R.id.title_button_back);
         travelnote_addtext1 = (Button) findViewById(R.id.add_text);
-        travelnote_addtext2 = (Button) findViewById(R.id.add_travelnotetitle_next);
-        travelnote_addimg = (Button) findViewById(R.id.add_img);
+        release_travelnote = (Button) findViewById(R.id.release_travelnote);
+        chooseFromAlbum = (Button) findViewById(R.id.choose_from_album);
+        picture = (ImageView) findViewById(R.id.picture);
         initView();
     }
 
     private void initView() {
         back.setOnClickListener(this);
         travelnote_addtext1.setOnClickListener(this);
-        travelnote_addtext2.setOnClickListener(this);
-        travelnote_addimg.setOnClickListener(this);
+        release_travelnote.setOnClickListener(this);
+        chooseFromAlbum.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.title_button_back:
-                onBackPressed();
+                startActivity(new Intent(Travelnote_add.this, TravelNoteActivity2.class));
                 break;
             case R.id.add_text:
-            case R.id.add_travelnotetitle_next:
                 startActivity(new Intent(Travelnote_add.this, Travelnote_addtext.class));
                 break;
-            case R.id.add_img:
-                openAlbum();
+            case R.id.release_travelnote:
+                Snackbar.make(v, "发表成功", Snackbar.LENGTH_SHORT)
+                        .setAction("查看", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(Travelnote_add.this, "此功能还未完善", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Travelnote_add.this, TravelNoteActivity2.class));
+                            }
+                        })
+                        .show();
+
+                break;
+            case R.id.choose_from_album:
+                if (ContextCompat.checkSelfPermission(Travelnote_add.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Travelnote_add.this, new String[]{ Manifest.permission. WRITE_EXTERNAL_STORAGE }, 1);
+                } else {
+                    openAlbum();
+                }
             default:
                 break;
         }
@@ -57,6 +95,93 @@ public class Travelnote_add extends AppCompatActivity implements View.OnClickLis
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction("android.intent.action.GET_CONTENT");
-        startActivityForResult(intent, 5002);
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    // 判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        // 4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    } else {
+                        // 4.4以下系统使用这个方法处理图片
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        Log.d("TAG", "handleImageOnKitKat: uri is " + uri);
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; // 解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是content类型的Uri，则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是file类型的Uri，直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath); // 根据图片路径显示图片
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        // 通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
     }
 }
