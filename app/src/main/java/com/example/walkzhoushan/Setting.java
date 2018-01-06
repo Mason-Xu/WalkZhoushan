5a658ce79400a43bb6c6c20d83c570200ce9b7eb
Mason-Xu <xulihao1997@gmail.com>
2018-01-06 15:36:36 +0800

    :sparkles: 程序退出dialog

--- app/src/main/java/com/example/walkzhoushan/Setting.java
+++ app/src/main/java/com/example/walkzhoushan/Setting.java
@@ -1,11 +1,15 @@
 package com.example.walkzhoushan;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import cn.bmob.v3.BmobUser;
 
 /**
  * Created by Mason on 2017/12/13.
@@ -43,7 +47,21 @@ public class Setting extends BaseActivityFinishAllActivity implements View.OnCli
                 onBackPressed();
                 break;
             case R.id.exitapp_button:
-                ActivityCollector.finishyAll();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Setting.this);
+                builder.setMessage("确定退出程序?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                ActivityCollector.finishyAll();
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(Setting.this,"取消退出程序",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.show();
                 break;
             default:
                 break;
